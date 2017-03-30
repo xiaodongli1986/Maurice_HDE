984ad819162065ae6a539c0d074b2788079c9ad2
xiaodongli <xiaodongli@kias.re.kr>
2017-03-30 16:55:54 +0900

    minor changes

---
+++ gitpush.sh
@@ -0,0 +1,11 @@
+git pull
+git add *.sh
+git add *.py
+git add *.ipynb
+git add src/*.f90
+git add src/Make*
+git add README.md
+git add Papers/*.pdf
+git add ImportantFigures/*.*
+git commit -m 'minor changes'
+git push
